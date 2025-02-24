720434ef1c4a6a8dd31017e0fce481b9042d6b2a
Ishan <102670414+1113Ishan@users.noreply.github.com>
2025-02-24 22:43:01 +0545

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWherehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
